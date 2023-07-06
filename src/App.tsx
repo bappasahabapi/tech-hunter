39c4949405edfd1a926ed6ccd084cbe64d374573
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-07-07 00:47:55 +0600

    router set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <h1>Hello tech hunter bapi</h1>
+      <h1 className="bg-green-500 text-2xl">Hello tech hunter bapi-- App.tsx is default routes</h1>
     </>
   )
 }
