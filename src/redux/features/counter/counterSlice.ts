fb3f538f644e12fcdc39f4f78283be5c1ac1a436
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-07-07 13:20:29 +0600

    hooks

--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,21 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-const initialState={
-    countValue:0,
-    // customized:{
-    //     num:5
-    // }
+export interface CounterState {
+    countValue: number
 }
+
+const initialState: CounterState = {
+    countValue: 0,
+}
+
+// const initialState={
+//     countValue:5,
+//     // customized:{
+//     //     num:0
+//     // }
+// }
 
 
 const counterSlice = createSlice({
     name: 'counter',
     initialState,
-    reducers:{
-        increment:(state)=>{state.countValue+=1},
-        decrement:(state)=>{state.countValue-=1},
-        // test:(state)=>{state.customized.num}
+    reducers: {
+        increment: (state) => { state.countValue += 1 },
+        decrement: (state) => { state.countValue -= 1 },
+        reset: (state) => { state.countValue = 0 },
+        // reset:(state)=>{state.customized.num}
+        incrementByAmount: (state, action: PayloadAction<number>) => {
+            state.countValue += action.payload
+        },
     },
 });
 
+// Action creators are generated for each case reducer function
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions
 export default counterSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,9 +5,9 @@ export const store = configureStore({
   reducer: {
     counter:counterReducer
   },
+  // devTools:false
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
